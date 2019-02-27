53f196e191659762d615ef10f0a55045911a6a10
zhaoxin <1835783944@qq.com>
2019-02-27 18:58:33 +0800

    正式提交冒泡排序

--- sort_algorithm/Bubble_sort/Bulle.cpp
+++ sort_algorithm/Bubble_sort/Bulle.cpp
@@ -1,8 +1,43 @@
 #include<iostream>
 using namespace std;
 
+void Swap(int& a,int& b)
+{
+  a = a - b;
+  b = a + b;
+  a = b - a;
+}
+
+void bulle_sort(int array[],size_t size)
+{
+  int i , j;
+  for(i = 0;i < size - 1;i++ )
+  {
+    for(j = 0;j < size-1-i;j++)
+    {
+      if(array[j] > array[j+1])
+      {
+        Swap(array[j],array[j+1]);
+      }
+    }
+  }
+}
+
+void Print(int array[],size_t size)
+{
+  int i;
+  for(i = 0 ;i < size ;i++ )
+  {
+    cout<<array[i]<<" ";
+  }
+  cout<<endl;
+}
+
 int main()
 {
-  cout<<"你好啊，败家娘们！"<<endl;
+  int array[10] = {1,3,5,7,8,9,0,6,4,2};
+  Print(array,10);
+  bulle_sort(array,10);
+  Print(array,10);
   return 0;
 }
