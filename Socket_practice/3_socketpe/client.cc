c2d8d1883292315b5dffab5225377dcda3bb45bb
root <1835783944@qq.com>
2019-03-14 09:21:07 +0800

    socket大写转小写

---
+++ Socket_practice/3_socketpe/client.cc
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<iostream>
+#include<stdlib.h>
+#include<string.h>
+#include<unistd.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<arpa/inet.h>
+
+#define MAXLINE 80
+#define SERV_PORT 6666
+
+using namespace std;
+int main(int argc,char *argv[])
+{
+	struct sockaddr_in servaddr;
+	char buf[MAXLINE];
+	int sockfd,n;
+	char *str;
+	
+	/*if(argc == 2)
+	{
+		fputs("usage:./client massage\n",stderr);
+		exit(1);
+		}*/
+	str = argv[1];
+
+	sockfd = socket(AF_INET,SOCK_STREAM,0);
+	
+	bzero(&servaddr,sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+	inet_pton(AF_INET,"127.17.98.161",&servaddr.sin_addr);
+	servaddr.sin_port = htons(SERV_PORT);
+
+	connect(sockfd,(struct sockaddr *)&servaddr,sizeof(servaddr));
+	
+	write(sockfd,str,strlen(str));
+	n=read(sockfd,buf,strlen(str));
+	cout<<"Response from server:"<<endl;
+
+	write(STDOUT_FILENO,buf,n);
+	close(sockfd);
+	return 0;
+	}
+
