185a663d1e8947b6d64deb7bf724da4043701ba9
root <1835783944@qq.com>
2019-03-15 09:24:33 +0800

    map的使用

--- STL_practice/map/main.cc
+++ STL_practice/map/main.cc
@@ -7,20 +7,31 @@ using namespace std;
 int main()
 {
 	map<char,int> m;
-	int i,n,tmp;
-	cin >> n;
-	for(i = 0;i < n;i++)
+	m['a'] = 20;
+	m['d'] = 60;
+	m['b'] = 40;
+	m['c'] = 10;
+
+	map<char,int>::iterator itt = m.find('c');
+	//cout<<it->first<<" "<<it->second<<endl;
+	
+	m.erase(itt,m.end());
+	m.clear();
+	
+	cout<<m.size()<<endl;
+	map<char,int>::iterator it = m.begin();
+	while(it != m.end())
 	{
-		cin>>tmp;
-		v.push_back(tmp);
+		cout<<it->first<<" "<<it->second<<endl;
+		it++;
 	}
 
-	vector<int>::iterator it = v.begin();
+	/*vector<int>::iterator it = v.begin();
 	while(it != v.end())
 	{
 		cout<<*it<<" ";
 		++it;
-	}
+	}*/
 	cout<<endl;
 	return 0;
 	}
