91c70af9c85d561d8d76899a4e5b1429fdc81b81
root <1835783944@qq.com>
2019-03-17 17:33:03 +0800

    判断三角形、求面积

---
+++ algorithm_practice/judge_triangle/triangle.cc
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+int main()
+{
+	int n, i;
+	float a, b, c;
+	float s, p;
+	scanf("%d", &n);
+	float arr[100] = { 0 };
+
+	for (i = 0; i<n * 3; i = i + 3)
+	{
+		scanf("%f%f%f", &a, &b, &c);
+		arr[i] = a;
+		arr[i + 1] = b;
+		arr[i + 2] = c;
+	}
+
+	for (i = 0; i<n * 3; i = i + 3)
+	{
+		a = arr[i];
+		b = arr[i + 1];
+		c = arr[i + 2];
+
+		if ((a>(b + c)) || (b>(a + c)) || c>(a + b))
+		{
+			printf("0.00\n");
+
+		}
+		else
+		{
+			p = (a + b + c) / 2;
+			s = sqrt(p*(p - a)*(p - b)*(p - c));
+			printf("%.2f\n", s);
+		}
+	}
+	return 0;
+}
