226ce63afe19fa61856413b0b9c519e384b73ed3
root <1835783944@qq.com>
2019-03-18 00:34:57 +0800

    C/S——socket通信完成

--- Socket_practice/3_socketpe/client.cc
+++ Socket_practice/3_socketpe/client.cc
@@ -7,38 +7,38 @@
 #include<netinet/in.h>
 #include<arpa/inet.h>
 
-#define MAXLINE 80
+#define BUFSIZE 800
 #define SERV_PORT 6666
+#define SERV_IP "172.17.98.161"
 
 using namespace std;
+
 int main(int argc,char *argv[])
 {
-	struct sockaddr_in servaddr;
-	char buf[MAXLINE];
+	struct sockaddr_in serv_addr;
+	//socket_t serv_addr_len;
 	int sockfd,n;
-	char *str;
-	
-	/*if(argc == 2)
-	{
-		fputs("usage:./client massage\n",stderr);
-		exit(1);
-		}*/
-	str = argv[1];
+	char buf[BUFSIZE];
 
 	sockfd = socket(AF_INET,SOCK_STREAM,0);
 	
-	bzero(&servaddr,sizeof(servaddr));
-	servaddr.sin_family = AF_INET;
-	inet_pton(AF_INET,"127.17.98.161",&servaddr.sin_addr);
-	servaddr.sin_port = htons(SERV_PORT);
+	memset(&serv_addr,0,sizeof(serv_addr));
 
-	connect(sockfd,(struct sockaddr *)&servaddr,sizeof(servaddr));
-	
-	write(sockfd,str,strlen(str));
-	n=read(sockfd,buf,strlen(str));
-	cout<<"Response from server:"<<endl;
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_port = htons(SERV_PORT);
+	inet_pton(AF_INET,SERV_IP,&serv_addr.sin_addr.s_addr);
 
-	write(STDOUT_FILENO,buf,n);
+	connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr));
+	while(1)
+	{
+		fgets(buf,sizeof(buf),stdin);//hello world --- fgets -->"hello world\n\0"
+
+		write(sockfd,buf,strlen(buf));
+
+		n=read(sockfd,buf,strlen(buf));
+
+		write(STDOUT_FILENO,buf,n);
+	}
 	close(sockfd);
 	return 0;
 	}
--- Socket_practice/3_socketpe/server.cc
+++ Socket_practice/3_socketpe/server.cc
@@ -3,6 +3,7 @@
 #include<sys/socket.h>
 #include<arpa/inet.h>
 #include<stdlib.h>
+#include<strings.h>
 #include<ctype.h>
 
 #define SERV_IP "127.0.0.1"
@@ -13,9 +14,13 @@ int main(void)
 	int lfd,cfd;
 	struct sockaddr_in serv_addr,clie_addr;
 	socklen_t clie_addr_len;
-	char buf[BUFSIZ];
+	char buf[BUFSIZ],clie_IP[BUFSIZ];
 	int n,i;
+
 	lfd = socket(AF_INET,SOCK_STREAM,0);
+	
+	bzero(&serv_addr,sizeof(serv_addr));//清空的缓冲区，大小；
+	
 	serv_addr.sin_family = AF_INET;
 	serv_addr.sin_port = htons(SERV_PORT);
 	serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
@@ -23,7 +28,11 @@ int main(void)
 	listen(lfd,128);
 
 	clie_addr_len = sizeof(clie_addr);
+	
 	cfd = accept(lfd,(struct sockaddr *)&clie_addr,&clie_addr_len);
+	printf("client IP:%s,client port:%d\n",
+	inet_ntop(AF_INET,&clie_addr.sin_addr.s_addr,clie_IP,sizeof(clie_IP)),
+	ntohs(clie_addr.sin_port));
 	while(1)
 	{
 		n=read(cfd,buf,sizeof(buf));
