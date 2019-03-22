c32fa4d8ccffd78c27af750a2c80c221005d64fa
root <1835783944@qq.com>
2019-03-22 08:25:26 +0800

    返回整数二进制1的个数

---
+++ algorithm_practice/return_1/test.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	
+	int n;
+    while(cin>>n)
+    {
+        int count = 0;
+        while(n)
+        {
+            if((n & 1) == 1)
+                count += 1;
+            n >>= 1;
+        }
+        cout << count <<endl;
+    }
+	return 0;
+}
